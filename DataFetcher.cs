24d3b3c4a47f95ca1f376ad99dbaf46e49a26ff9
kosiyyu <kosiyyu@gmail.com>
2023-04-10 02:38:11 +0200

    Add project files.

---
+++ DataFetcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CryptoConsole
+{
+    internal static class DataFetcher
+    {
+        private const string url = @"https://api.wazirx.com/sapi/v1/tickers/24hr";
+        private static readonly HttpClient s_httpClient = new HttpClient();
+        private static readonly JsonSerializerOptions s_jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        public static async Task<List<CryptoDto>> Fetch()
+        {
+            var response = await s_httpClient.GetAsync(url);
+
+            if(response == null || !response.IsSuccessStatusCode)
+            {
+                return new List<CryptoDto>();
+            }
+
+            var content = await response.Content.ReadAsStringAsync();//that works;)))
+            //await Console.Out.WriteLineAsync(content);
+            var deserializedDataList = JsonSerializer.Deserialize<List<Crypto>>(content, s_jsonSerializerOptions).ToList();
+            var dtos = deserializedDataList.Select(crypto => new CryptoDto
+            {
+                Symbol = crypto.Symbol,
+                BaseAsset = crypto.BaseAsset,
+                QuoteAsset = crypto.QuoteAsset,
+                OpenPrice = Convert.ToDouble(crypto.OpenPrice),
+                LowPrice = Convert.ToDouble(crypto.LowPrice),
+                HighPrice = Convert.ToDouble(crypto.HighPrice),
+                LastPrice = Convert.ToDouble(crypto.LastPrice),
+                Volume = Convert.ToDouble(crypto.Volume),
+                BidPrice = Convert.ToDouble(crypto.BidPrice),
+                AskPrice = Convert.ToDouble(crypto.AskPrice)
+            }
+            ).ToList();
+
+            foreach( var d in dtos)
+            {
+                await Console.Out.WriteLineAsync(d.ToString());
+            }
+
+            return dtos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using CryptoConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<CryptoDto> list = Task.Run(async () => await DataFetcher.Fetch()).GetAwaiter().GetResult();
+
+        foreach (CryptoDto i in list)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+/*    static async Task Main(string[] args)
+    {
+        var list = await DataFetcher.Fetch();
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item.At);
+        }
+
+        // do other stuff...
+    }*/
+}
